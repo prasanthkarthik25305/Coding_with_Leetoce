8220efaca23fd6d0d676bc2d9dfd83d1371e1db6
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2024-09-21 14:24:43 +0530

    Time: 11 ms (65.70%) | Memory: 13.6 MB (76.44%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+  void generatelexy(int curr,int limit,vector<int>&ans){
+    // if curr is less than limit
+    if(ans.size()>=limit) return;
+    if(curr>limit) return;
+    ans.push_back(curr);
+    for(int i=0;i<=9;i++){
+        int next=curr*10+i;
+        generatelexy(next,limit,ans);
+    }
+
+  }
     vector<int> lexicalOrder(int n) {
-        vector<string>res;
-        for(int i=1;i<=n;i++){
-            res.push_back(to_string(i));
+        vector<int>res;
+        for(int start=1;start<=n;start++){
+          generatelexy(start,n,res);
         }
-        sort(res.begin(),res.end());
-        vector<int>ans;
-        for(auto i:res){
-            int k=stoi(i);
-            ans.push_back(k);
-        }
-        return ans;
+        return res;
     }
 };
