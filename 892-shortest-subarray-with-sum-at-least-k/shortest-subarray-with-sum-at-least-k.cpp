dc61e3cf8c8b0535b4fb52d04d5040cc8d07ce62
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2025-01-23 22:45:38 +0530

    Time: 1627 ms (5.06%) | Memory: 174.6 MB (5.01%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        #define int long long
+       // #define int long long
        int sum=0,ans=nums.size()+1;
-       vector<int>prefix(nums.size()+1,0);
+       vector<long long>prefix(nums.size()+1,0);
        for(int i=1;i<=nums.size();i++){
        prefix[i]=prefix[i-1]+nums[i-1];
        // else prefix[i]=nums[i];
        }
-       set<pair<int,int>>st;
+       set<pair<long long,int>>st;
        for(int i=0,j=0;i<=nums.size();i++){
           while(!st.empty() and prefix[i]-(st.begin()->first)>=k){
             ans=min(ans,i-st.begin()->second);
@@ -25,6 +25,6 @@ public:
           st.insert({prefix[i],i});
        } 
        return (ans!=nums.size()+1)?ans:-1;
-       #undef int
+     //  #undef int
     }
 };
