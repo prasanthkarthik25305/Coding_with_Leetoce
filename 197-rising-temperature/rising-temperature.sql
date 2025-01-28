95f38c0927ef6ca96898cd8e33291bc376ea4647
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2025-01-28 10:19:45 +0530

    Time: 910 ms (49.70%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select w2.id Id from Weather w1 join Weather w2 on datediff(w2.recordDate,w1.recordDate)=1 and w1.temperature<w2.temperature;
+/* Write your PL/SQL query statement below */
+select w2.id Id from Weather w1 join Weather w2 on w2.recordDate-w1.recordDate=1 and w1.temperature<w2.temperature;
