de8c60571b6829a9c40c54505f698371dde3ef39
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2025-02-02 17:45:32 +0530

    Time: 257 ms (7.34%) | Memory: 27.5 MB (93.44%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int>h(m, 0);
+        int side,maxarea = 0;
+        for(auto &row:matrix){
+            for(int i=0;i<row.size();i++){
+                h[i]=(row[i]=='1')?h[i]+1:0;
+            }
+            // for(auto &it : h) cout << it <<" ";
+            // cout <<"\n";
+            for(int i=0;i<row.size();i++){
+                int left=i;
+                for(int x=i;x>0 and h[x-1]>=h[i];x--){
+                    left--;
+                }
+                int right=i;
+                for(int x=i;x<row.size()-1 and h[x+1]>=h[i];x++){
+                    right++;
+                }
+                 side=min(h[i],right-left+1);
+                 maxarea=max(maxarea,side*side);
+            }    
+        }
+         return maxarea;
+    }
+};
