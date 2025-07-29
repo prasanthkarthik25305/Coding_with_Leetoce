c6cc2f7cb9e8e00c0a01ea0d0b8f4899e711964c
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2025-07-29 15:54:48 +0530

    Time: 82 ms (8.68%) | Memory: 82.4 MB (14.08%) - LeetSync

---
+++ 3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    #define ll long long 
+    ll CntLCTs(string s){
+        int n=s.size();
+      vector<int>prefix(n+1,0);
+      vector<int>suffix(n+1,0);
+      for(int i=1;i<n;i++){
+        if(s[i-1]=='L') prefix[i+1]=1;
+      }
+      //for suffix
+      for(int i=n-2;i>=0;i--){
+        if(s[i+1]=='T') suffix[i+1]=1;
+      }
+      for(int i=1;i<=n;i++){
+           prefix[i]+=prefix[i-1];
+      }   
+      // for suffix
+      for(int i=n-1;i>=1;i--){
+        suffix[i]+=suffix[i+1];
+      }
+     ll ans=0;
+     for(int i=0;i<n;i++){
+        if(s[i]=='C'){
+            ans+=prefix[i+1]*suffix[i+1];
+        }
+     }
+     return ans;
+    
+    }
+    long long numOfSubsequences(string s) {
+      string s1="L";
+      s1+=s;
+      string s2=s;
+      s2+="T";
+      ll ans1=CntLCTs(s1);
+      ll ans2=CntLCTs(s2);
+
+      ll ans3=0;
+
+       int n=s.size();
+      vector<ll>prefix(n+1,0);
+      vector<ll>suffix(n+1,0);
+      for(int i=0;i<n;i++){
+        if(s[i]=='L') prefix[i+1]=1;
+      }
+      //for suffix
+      for(int i=n-1;i>=0;i--){
+        if(s[i]=='T') suffix[i+1]=1;
+      }
+      for(int i=1;i<=n;i++){
+           prefix[i]+=prefix[i-1];
+      }   
+      // for suffix
+      for(int i=n-1;i>=1;i--){
+        suffix[i]+=suffix[i+1];
+      }
+      ll maxy=0;
+     for(int i=0;i<n;i++){
+        if(s[i]!='C'){
+            maxy=max(maxy,prefix[i+1]*suffix[i+1]);
+        }
+        else ans3+=prefix[i+1]*suffix[i+1];
+     }
+     ans3+=maxy;
+      return max(ans1,max(ans2,ans3));
+    }
+};
