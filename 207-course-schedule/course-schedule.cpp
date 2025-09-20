7cac5a90ca1aa40aae53fc2f15733befa3728ea7
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2025-09-20 19:10:55 +0530

    Time: 1 ms (87.04%) | Memory: 19.3 MB (74.62%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& state) {
-        if (state[node] == 1) return true;   // cycle found
-        if (state[node] == 2) return false;  // already visited, no cycle
+    bool dfs(int node,vector<vector<int>>&adj,vector<int>&state) {
+        if(state[node]==1) return true;
+        if(state[node]==2) return false;
 
-        state[node] = 1;  // mark as visiting
-
-        for (int nei : adj[node]) {
-            if (dfs(nei, adj, state)) return true;
+        state[node]=1;
+        for(auto nei:adj[node]) {
+           if(dfs(nei,adj,state)) return true;
         }
-
-        state[node] = 2;  // mark as fully visited
-        return false;
+        state[node]=2;
+       return false;
     }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
 
-        // build adjacency list (b -> a)
-        for (auto& p : prerequisites) {
-            adj[p[1]].push_back(p[0]);
+        vector<vector<int>>adj(numCourses);
+        for(auto &p:prerequisites){
+            adj[p[0]].push_back(p[1]);
         }
-
-        vector<int> state(numCourses, 0); // 0 = not visited, 1 = visiting, 2 = visited
-
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) {
-                if (dfs(i, adj, state)) return false; // cycle detected
+       
+        vector<int>state(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+              if(state[i]==0) if(dfs(i,adj,state)) return false;
             }
-        }
-        return true; // no cycle
+        return true;
     }
 };
