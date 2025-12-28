360cf2400f47d82249d8954d296976ac6708b27e
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2025-12-28 21:45:37 +0530

    Time: 1102 ms (6.90%) | Memory: 204.1 MB (6.90%) - LeetSync

--- 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.cpp
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.cpp
@@ -6,17 +6,13 @@ public:
         int cnt=0;
         if(k==2 or k==5) return -1;
         bool chk=true;
-        ll r=0;
+        ll r=1%k;
         map<int,int>mp;
         while(chk){
-            r=n%k;
+            if(r==0) chk=false;
+            r=(r*10+1)%k;
             if(mp[r]>0) return -1;
             mp[r]++;
-            if(r==0){
-                chk=false;
-            }
-            n=r*10+1;
-
             //cout<<n<<' ';
             cnt++;
         }
