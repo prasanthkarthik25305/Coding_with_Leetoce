0861ec635d8e32409e9638389408318161fdc05e
Prasanth <126947745+prasanthkarthik25305@users.noreply.github.com>
2026-02-01 22:27:00 +0530

    Time: 63 ms (27.39%) | Memory: 148.5 MB (32.69%) - LeetSync

---
+++ 4118-design-ride-sharing-system/design-ride-sharing-system.cpp
@@ -0,0 +1,55 @@
+class RideSharingSystem {
+public:
+    queue<int> riders, drivers;
+    unordered_set<int> cancelled;
+    unordered_set<int> matched;
+    unordered_set<int> waiting;
+
+    RideSharingSystem() {}
+
+    void addRider(int riderId) {
+        riders.push(riderId);
+        waiting.insert(riderId);
+    }
+
+    void addDriver(int driverId) {
+        drivers.push(driverId);
+    }
+
+    vector<int> matchDriverWithRider() {
+        // remove cancelled riders lazily
+        while (!riders.empty() && cancelled.count(riders.front())) {
+            cancelled.erase(riders.front());
+            waiting.erase(riders.front());
+            riders.pop();
+        }
+
+        if (drivers.empty() || riders.empty())
+            return {-1, -1};
+
+        int d = drivers.front(); drivers.pop();
+        int r = riders.front(); riders.pop();
+
+        matched.insert(r);
+        waiting.erase(r);
+
+        return {d, r};
+    }
+
+    void cancelRider(int riderId) {
+        // cancel ONLY if rider is currently waiting
+        if (waiting.count(riderId)) {
+            cancelled.insert(riderId);
+        }
+    }
+};
+
+
+/**
+ * Your RideSharingSystem object will be instantiated and called as such:
+ * RideSharingSystem* obj = new RideSharingSystem();
+ * obj->addRider(riderId);
+ * obj->addDriver(driverId);
+ * vector<int> param_3 = obj->matchDriverWithRider();
+ * obj->cancelRider(riderId);
+ */
